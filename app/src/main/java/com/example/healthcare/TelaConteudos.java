d32850756730067845274ce24b6ab5c0acd63452
theobarretosilva <barretotheo25@gmail.com>
2022-07-29 22:12:26 -0300

    upload

--- app/src/main/java/com/example/healthcare/TelaConteudos.java
+++ app/src/main/java/com/example/healthcare/TelaConteudos.java
@@ -1,11 +1,23 @@
 package com.example.healthcare;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class TelaConteudos extends AppCompatActivity {
+
+    TextView olaUsu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +25,26 @@ public class TelaConteudos extends AppCompatActivity {
         setContentView(R.layout.tela_conteudos);
         getWindow().setStatusBarColor(Color.rgb(12,92,100));
         getSupportActionBar().hide();
+
+        olaUsu = findViewById(R.id.olaUsu);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        String usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = db.collection("Usuarios").document(usuarioID).collection("Informações pessoais").document("Informações de cadastro");
+        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                if (documentSnapshot != null){
+                    String nomeUsu = documentSnapshot.getString("Nome");
+                    String olaUsuCompleto = "Olá, " + nomeUsu + "!";
+                    olaUsu.setText(olaUsuCompleto);
+                }
+            }
+        });
     }
 }
