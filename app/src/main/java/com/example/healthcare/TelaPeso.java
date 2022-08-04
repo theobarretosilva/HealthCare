5831da50c33b8d305b0dae42577853f1e31cd75c
Théo Barreto Silva <barretotheo25@gmail.com>
2022-08-04 09:50:51 -0300

    Atualizando projeto

--- app/src/main/java/com/example/healthcare/TelaPeso.java
+++ app/src/main/java/com/example/healthcare/TelaPeso.java
@@ -2,6 +2,7 @@ package com.example.healthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 public class TelaPeso extends AppCompatActivity {
@@ -10,5 +11,7 @@ public class TelaPeso extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela_peso);
+        getWindow().setStatusBarColor(Color.rgb(12,92,100));
+        getSupportActionBar().hide();
     }
 }
