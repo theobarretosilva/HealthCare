246dd1677189517fd8ece8df76a99b8868a96785
theobarretosilva <barretotheo25@gmail.com>
2022-08-10 23:29:43 -0300

    upload

--- app/src/main/java/com/example/healthcare/TelaVacinas.java
+++ app/src/main/java/com/example/healthcare/TelaVacinas.java
@@ -1,14 +1,49 @@
 package com.example.healthcare;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class TelaVacinas extends AppCompatActivity {
+
+    TextView olaNomeUsu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela_vacinas);
+        getWindow().setStatusBarColor(Color.rgb(12,92,100));
+        getSupportActionBar().hide();
+
+        olaNomeUsu = findViewById(R.id.olaNomeUsu);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        String usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = db.collection("Usuarios").document(usuarioID).collection("Informações pessoais").document("Informações de cadastro");
+        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                if (documentSnapshot != null){
+                    String nomeUsu = documentSnapshot.getString("Nome");
+                    String olaUsuCompleto = "Olá, " + nomeUsu + "!";
+                    olaNomeUsu.setText(olaUsuCompleto);
+                }
+            }
+        });
     }
 }
