339a9630502209ddc1c99f070513f5f789373dfd
Théo Barreto Silva <barretotheo25@gmail.com>
2022-08-19 22:43:48 -0300

    Fazendo tela de perfil

--- app/src/main/java/com/example/healthcare/TelaPerfil.java
+++ app/src/main/java/com/example/healthcare/TelaPerfil.java
@@ -2,10 +2,17 @@ package com.example.healthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class TelaPerfil extends AppCompatActivity {
+
+    Button btnVoltar, btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +20,24 @@ public class TelaPerfil extends AppCompatActivity {
         setContentView(R.layout.tela_perfil);
         getWindow().setStatusBarColor(Color.rgb(12,92,100));
         getSupportActionBar().hide();
+
+        btnVoltar = findViewById(R.id.btnVoltar);
+        btnLogout = findViewById(R.id.btnLogout);
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent voltarTelaConteudos = new Intent(TelaPerfil.this, TelaConteudos.class);
+                startActivity(voltarTelaConteudos);
+            }
+        });
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent logout = new Intent(TelaPerfil.this, TelaInicial.class);
+                startActivity(logout);
+            }
+        });
     }
 }
