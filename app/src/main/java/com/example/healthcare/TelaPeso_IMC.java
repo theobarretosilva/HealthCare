8db2f8b7ac92644ee83ab7843b720e77421af078
Théo Barreto Silva <barretotheo25@gmail.com>
2022-08-19 21:53:13 -0300

    Atualizando projeto

--- app/src/main/java/com/example/healthcare/TelaPeso_IMC.java
+++ app/src/main/java/com/example/healthcare/TelaPeso_IMC.java
@@ -3,10 +3,8 @@ package com.example.healthcare;
 import static java.lang.Integer.parseInt;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,8 +13,14 @@ import android.widget.TextView;
 public class TelaPeso_IMC extends AppCompatActivity {
 
     TextView btnHomem, btnMulher, btnMenos, btnMais, idade;
+    Button btn;
 
-    int idadePessoa = 0;
+    Integer idadePessoa;
+
+    private void atualizaContador() {
+        idade = findViewById(R.id.idade);
+        idade.setText(idadePessoa.toString());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +31,26 @@ public class TelaPeso_IMC extends AppCompatActivity {
 
         btnHomem = findViewById(R.id.btnHomem);
         btnMulher = findViewById(R.id.btnMulher);
-        btnMenos = findViewById(R.id.menos);
-        btnMais = findViewById(R.id.mais);
-        idade = findViewById(R.id.idade);
+        btnMenos = findViewById(R.id.btnMenos);
+        btnMais = findViewById(R.id.btnMais);
+
+        idadePessoa = 0;
+
+        btnMenos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                idadePessoa -= 1;
+                atualizaContador();
+            }
+        });
+        btnMais.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                idadePessoa += 1;
+                atualizaContador();
+
+            }
+        });
 
     }
 
@@ -41,12 +62,6 @@ public class TelaPeso_IMC extends AppCompatActivity {
     public void mudarSexoMulher(View m){
         btnHomem.setBackgroundResource(R.drawable.container_sexo1_imc);
         btnMulher.setBackgroundResource(R.drawable.container_sexo2_imc);
-    }
-
-    public void aumentarIdade(View a){
-        idadePessoa++;
-
-        idade.setText(idadePessoa);
     }
 
 }
