93eb71c6b7ff1f642759e32e85fea74ab99335be
Théo Barreto Silva <barretotheo25@gmail.com>
2022-08-31 08:26:35 -0300

    Iniciando design tela de adicionar cafe da manha

--- app/src/main/java/com/example/healthcare/TelaInicial.java
+++ app/src/main/java/com/example/healthcare/TelaInicial.java
@@ -28,7 +28,7 @@ public class TelaInicial extends AppCompatActivity {
         getWindow().setStatusBarColor(Color.rgb(12,92,100));
         getSupportActionBar().hide();
 
-        Intent irDireto = new Intent (this, TelaAlimentacao.class);
+        Intent irDireto = new Intent (this, TelaAdicionarCafe_da_manha.class);
         startActivity(irDireto);
 
         login = findViewById(R.id.login);
