547e87ace2a8f22ee8bce372e6dbffb358dd054c
Théo Barreto Silva <barretotheo25@gmail.com>
2022-09-01 20:25:17 -0300

    Design TelaPassos pronto;

--- app/src/main/java/com/example/healthcare/TelaPassos.java
+++ app/src/main/java/com/example/healthcare/TelaPassos.java
@@ -6,8 +6,18 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TelaPassos extends AppCompatActivity {
+
+    TextView dataAtual;
+
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    Date data = new Date();
+    String dataHoje = sdf.format(data);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +25,16 @@ public class TelaPassos extends AppCompatActivity {
         setContentView(R.layout.tela_passos);
         getWindow().setStatusBarColor(Color.rgb(12,92,100));
         getSupportActionBar().hide();
+
+        dataAtual = findViewById(R.id.dataAtualPassos);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        String textoPronto = "\uD83C\uDFC3\uD83C\uDFFD " + dataHoje;
+
+        dataAtual.setText(textoPronto);
     }
 
     public void voltarTelaConteudos(View g){
