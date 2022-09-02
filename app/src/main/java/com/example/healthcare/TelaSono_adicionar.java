95c164893363e76d7ed67342332db0e4987d9dcd
Théo Barreto Silva <barretotheo25@gmail.com>
2022-09-02 09:06:33 -0300

    Comecei o menu

---
+++ app/src/main/java/com/example/healthcare/TelaSono_adicionar.java
@@ -0,0 +1,14 @@
+package com.example.healthcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaSono_adicionar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_sono_adicionar);
+    }
+}
