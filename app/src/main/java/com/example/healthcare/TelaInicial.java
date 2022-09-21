7f249c21287d6cdba186f0e0886ebe9f13da4f4e
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2022-09-21 12:37:06 -0300

    Contador de passos

--- app/src/main/java/com/example/healthcare/TelaInicial.java
+++ app/src/main/java/com/example/healthcare/TelaInicial.java
@@ -26,8 +26,8 @@ public class TelaInicial extends AppCompatActivity {
         getWindow().setStatusBarColor(Color.rgb(12,92,100));
         getSupportActionBar().hide();
 
-        Intent irDireto = new Intent (this, TelaConteudos_Premium.class);
-        startActivity(irDireto);
+//        Intent irDireto = new Intent (this, TelaConteudos_Premium.class);
+//        startActivity(irDireto);
 
         login = findViewById(R.id.login);
         cadastrar = findViewById(R.id.cadastrar);
--- app/src/main/java/com/example/healthcare/TelaPassos.java
+++ app/src/main/java/com/example/healthcare/TelaPassos.java
@@ -1,9 +1,12 @@
 package com.example.healthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -27,6 +30,7 @@ public class TelaPassos extends AppCompatActivity implements SensorEventListener
     private SensorManager mSensorManager;
     private Sensor mStepDetectorSensor;
     int totalPassos = 0;
+//    int requestCode = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +44,12 @@ public class TelaPassos extends AppCompatActivity implements SensorEventListener
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mStepDetectorSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+
+//        if(ContextCompat.checkSelfPermission(this,
+//                Manifest.permission.HIGH_SAMPLING_RATE_SENSORS) == PackageManager.PERMISSION_DENIED){
+//            //ask for permission
+//            requestPermissions(new String[]{Manifest.permission.HIGH_SAMPLING_RATE_SENSORS}, requestCode );
+//        }
 
         setarData();
     }
@@ -68,9 +78,6 @@ public class TelaPassos extends AppCompatActivity implements SensorEventListener
             //Detectou passo
             totalPassos ++;
             dectaPasso.setText(""+totalPassos);
-        }
-        if(sensor.getType() == Sensor.TYPE_STEP_COUNTER){
-            //Alterou contagem de passos
         }
 
     }
