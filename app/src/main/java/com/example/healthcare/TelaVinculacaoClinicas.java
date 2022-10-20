547ed060930b7ad9570bb87c5c534e3059a5672c
Théo Barreto Silva <barretotheo25@gmail.com>
2022-10-20 17:56:37 -0300

    vinculação de clinicas funcionando completamente

--- app/src/main/java/com/example/healthcare/TelaVinculacaoClinicas.java
+++ app/src/main/java/com/example/healthcare/TelaVinculacaoClinicas.java
@@ -4,17 +4,25 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.firebase.database.DatabaseReference;
 
 public class TelaVinculacaoClinicas extends AppCompatActivity {
 
     TextView nomeClinica, enderecoClinica, telefoneClinica, servicosClinica;
     ImageView fotoClinica;
-
-    TelaClinicas telaClinicas;
+    CheckBox termos;
+    BottomSheetDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +41,12 @@ public class TelaVinculacaoClinicas extends AppCompatActivity {
         telefoneClinica = findViewById(R.id.telefoneClinica);
         servicosClinica = findViewById(R.id.servicosClinicas);
         fotoClinica = findViewById(R.id.fotoClinica);
+        termos = findViewById(R.id.checkTermos);
+        dialog = new BottomSheetDialog(this);
     }
 
     public void getSetClinica(){
-        System.out.println(telaClinicas.lClinicas);
-        for (VincularClinicas c : telaClinicas.lClinicas){
+        for (VincularClinicas c : TelaClinicas.lClinicas){
             nomeClinica.setText(c.getNomeClinica());
             enderecoClinica.setText(c.getEnderecoClinica());
             telefoneClinica.setText(c.getTelefoneClinica());
@@ -49,5 +58,48 @@ public class TelaVinculacaoClinicas extends AppCompatActivity {
     public void voltarTela(View g){
         Intent voltarTela = new Intent(this, TelaClinicas.class);
         startActivity(voltarTela);
+    }
+
+    public void mostrarCard(){
+        View view = getLayoutInflater().inflate(R.layout.card_clinica_vinculada, null, false);
+
+        Button voltarTelaClinicas = view.findViewById(R.id.btnOk);
+        voltarTelaClinicas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                voltarTela(view);
+            }
+        });
+
+        dialog.setContentView(view);
+        dialog.show();
+    }
+
+    public void vincularClinica(View v){
+
+        if (termos.isChecked()){
+
+            for (VincularClinicas c : TelaClinicas.lClinicas){
+
+                DatabaseReference reference = FirebaseHelper.getDatabaseReference()
+                        .child("Registros")
+                        .child(FirebaseHelper.getUIDUsuario())
+                        .child("Clínicas vinculadas")
+                        .child(c.getNomeClinica());
+
+                reference.setValue(c.getNomeClinica()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        mostrarCard();
+                    }
+
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+            }
+        }
     }
 }
