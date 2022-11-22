456ca212ee9d32c5d80c5072bb4d2f7001c15d43
Théo Barreto Silva <barretotheo25@gmail.com>
2022-11-22 12:35:10 -0300

    upload

---
+++ app/src/main/java/com/example/healthcare/AdapterExercicios.java
@@ -0,0 +1,8 @@
+package com.example.healthcare;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AdapterExercicios extends RecyclerView.Adapter<AdapterExercicios.MyViewHolder> {
+
+    private List<Exer'>
+}
---
+++ app/src/main/java/com/example/healthcare/Exercicio.java
@@ -0,0 +1,4 @@
+package com.example.healthcare;
+
+public class Exercicio {
+}
