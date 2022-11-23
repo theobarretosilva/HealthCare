99d54242f0004a39d5a7957ab56eba4f6a305265
Théo Barreto Silva <barretotheo25@gmail.com>
2022-11-23 18:33:48 -0300

    fazendo tela de medicamentos

--- app/src/main/java/com/example/healthcare/TelaMedicamentos.java
+++ app/src/main/java/com/example/healthcare/TelaMedicamentos.java
@@ -5,11 +5,26 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.ActivityOptionsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TelaMedicamentos extends AppCompatActivity {
+
+    private AdapterMedicamento adapterMedicamento;
+    private List<Medicamento> medicamentoList = new ArrayList<>();
+    private RecyclerView rvMedicamentos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +32,11 @@ public class TelaMedicamentos extends AppCompatActivity {
         setContentView(R.layout.tela_medicamentos);
         getWindow().setStatusBarColor(Color.rgb(12,92,100));
         getSupportActionBar().hide();
+
+        rvMedicamentos = findViewById(R.id.rvMedicamentos);
+
+        configRecyclerView();
+        recuperaMedicamentos();
     }
 
     public void voltarTela(View h){
@@ -31,5 +51,36 @@ public class TelaMedicamentos extends AppCompatActivity {
 
     public void irAddMedicamentos(View r){
         startActivity(new Intent(TelaMedicamentos.this, TelaMedicamentos_add.class));
+    }
+
+    private void configRecyclerView(){
+        rvMedicamentos.setLayoutManager(new LinearLayoutManager(this));
+        rvMedicamentos.setHasFixedSize(true);
+        adapterMedicamento = new AdapterMedicamento(medicamentoList);
+        rvMedicamentos.setAdapter(adapterMedicamento);
+    }
+
+    public void recuperaMedicamentos(){
+        DatabaseReference medicamentosRef = FirebaseHelper.getDatabaseReference()
+                .child("Registros")
+                .child(FirebaseHelper.getUIDUsuario())
+                .child("Medicamentos");
+        medicamentosRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    for (DataSnapshot snap : snapshot.getChildren()){
+                        Medicamento medicamento = snap.getValue(Medicamento.class);
+                        medicamentoList.add(medicamento);
+                    }
+                    adapterMedicamento.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
